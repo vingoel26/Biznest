5ea6cc0fc82277828e3dbbf2d89cd9db1ec8cd55
vingoel26 <vinayakgoel012@gmail.com>
2025-05-01 04:22:19 +0530

    minor change

--- Biznest/src/App.jsx
+++ Biznest/src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 import LoginPage from './components/LoginPage';
 import Dashboard from './components/Dashboard';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
-<Dashboard />
+<HomePage />
   );
 }
 
